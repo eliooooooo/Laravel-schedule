20ce9a020aec74f6cf888772a71f1270de567c28
eliooooooo <eliott.burkle@gmail.com>
2024-09-12 17:51:34 +0200

    Feat (models): student-formation relationship in models

--- app/Models/Formation.php
+++ app/Models/Formation.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Formation extends Model
 {
     use HasFactory;
+
+    public function students(){
+        return $this->hasMany(Student::class);
+    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,8 +21,8 @@ class DatabaseSeeder extends Seeder
         ]);
 
         $this->call([
-            StudentSeeder::class,
             FormationSeeder::class,
+            StudentSeeder::class,
         ]);
     }
 }
