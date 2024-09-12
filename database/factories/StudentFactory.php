cba4b0ddc62905d1b2a11561048e4b9e75de879e
eliooooooo <eliott.burkle@gmail.com>
2024-09-12 14:18:55 +0200

    Feat: first migration, database handle and factories

---
+++ database/factories/StudentFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Student>
+ */
+class StudentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'lastname' => fake()->lastName(),
+            'firstname' => fake()->firstName(),
+            'number' => fake()->randomNumber(6),
+            'email' => fake()->safeEmail(),
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', function(){
+    return view('test');
+});
+
+Route::get('/greeting/{name}', function(string $name){
+    return view('greeting', ['name' => $name]);
+});
