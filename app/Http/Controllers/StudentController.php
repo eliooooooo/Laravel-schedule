0ea2af6fa8d2abfabf8bddc3e53419258e7cd345
eliooooooo <eliott.burkle@gmail.com>
2024-09-12 16:53:15 +0200

    Feat (controller): create student controller

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    public function index() {
+        $students = Student::orderBy('lastname')->orderBy('firstname')->get();
+        return view('student.index', ['students' => $students]);
+    }
+
+    public function show(Student $student) {
+        return view('student.show', ['student' => $student]);
+    }
+}
