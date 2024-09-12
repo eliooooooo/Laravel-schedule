afb19d70a6a644283200a12b1964d08b92743fb8
eliooooooo <eliott.burkle@gmail.com>
2024-09-12 15:20:15 +0200

    Feat: Formations table handler

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             StudentSeeder::class,
+            FormationSeeder::class,
         ]);
     }
 }
---
+++ database/seeders/FormationSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Formation;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class FormationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $formations = ['MMI1', 'MMI2', 'MMI3', 'QLIO1', 'QLIO2', 'QLIO3', 'GEII1', 'GEII2', 'GEII3'];
+        foreach ($formations as $formation) {
+            Formation::create([
+                'name' => $formation,
+            ]);
+        }
+    }
+}
